eb48d30121205f33aeaa15b95a6aad77bff82001
Benedykt <benedykt.huszcza@gmail.com>
2022-11-09 07:28:24 +0100

    add chessmanId parameter

---
+++ src/King.java
@@ -0,0 +1,6 @@
+public class King implements Chessman{
+    @Override
+    public Boolean makeMove(String position, String[][] board, Boolean isWhite, String chessmanId) {
+        return null;
+    }
+}
